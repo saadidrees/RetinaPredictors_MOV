1c4e5834daf3d517bea92e701167067bc6674b65
Saad Idrees <idrees.sa@gmail.com>
2022-01-31 14:04:24 -0500

    parameterized learning rate

--- grid_scripts/cnn3d_launcher.sh
+++ grid_scripts/cnn3d_launcher.sh
@@ -15,16 +15,14 @@ module load python/3.6 cuda cudnn
 cd $SLURM_TMPDIR
 echo "directory changed"
 echo $PWD
-mkdir RetinaPredictors
-cp -r ~/scratch/RetinaPredictors/grid_scripts/from_git/ $SLURM_TMPDIR/
+mkdir RetinaPredictors_MOV
+cp -r ~/scratch/RetinaPredictors_MOV/grid_scripts/from_git/ $SLURM_TMPDIR/
 cd $SLURM_TMPDIR/from_git
 echo "directory changed"
 echo $PWD
 ls -l
 echo "files copied"
 
-#cp /home/sidrees/scratch/dynamic_retina/models/cluster_run_model_cnn3d.py $SLURM_TMPDIR/
-#cp -R /home/sidrees/scratch/dynamic_retina/models/model $SLURM_TMPDIR/
 
 virtualenv --no-download  $SLURM_TMPDIR/tensorflow_env
 source $SLURM_TMPDIR/tensorflow_env
@@ -32,4 +30,4 @@ pip install --no-index scipy matplotlib tensorflow_gpu
 
 
 
-python run_model_cnn3d.py $expDate $mdl_name $path_model_save_base $fname_data_train_val_test --path_existing_mdl=$path_existing_mdl --runOnCluster=$runOnCluster --chan1_n=$chan1_n --filt1_size=$filt1_size --filt1_3rdDim=$filt1_3rdDim --chan2_n=$chan2_n --filt2_size=$filt2_size --filt2_3rdDim=$filt2_3rdDim --chan3_n=$chan3_n --filt3_size=$filt3_size --filt3_3rdDim=$filt3_3rdDim --nb_epochs=$nb_epochs --thresh_rr=$thresh_rr --temporal_width=$temporal_width --pr_temporal_width=$pr_temporal_width --bz_ms=$bz_ms --BatchNorm=$BatchNorm --MaxPool=$MaxPool --c_trial=$c_trial --USE_CHUNKER=$use_chunker --trainingSamps_dur=$TRSAMPS
+python run_models.py $expDate $mdl_name $path_model_save_base $fname_data_train_val_test --path_existing_mdl=$path_existing_mdl --runOnCluster=$runOnCluster --chan1_n=$chan1_n --filt1_size=$filt1_size --filt1_3rdDim=$filt1_3rdDim --chan2_n=$chan2_n --filt2_size=$filt2_size --filt2_3rdDim=$filt2_3rdDim --chan3_n=$chan3_n --filt3_size=$filt3_size --filt3_3rdDim=$filt3_3rdDim --nb_epochs=$nb_epochs --thresh_rr=$thresh_rr --temporal_width=$temporal_width --pr_temporal_width=$pr_temporal_width --bz_ms=$bz_ms --BatchNorm=$BatchNorm --MaxPool=$MaxPool --c_trial=$c_trial --USE_CHUNKER=$use_chunker --trainingSamps_dur=$TRSAMPS --lr=$lr
--- grid_scripts/createJobs_csvParams_cnn3d.sh
+++ grid_scripts/createJobs_csvParams_cnn3d.sh
@@ -10,7 +10,7 @@ cd ..
 echo "Fetched latest files from git"
 
 PARAMS_FILE="model_params.csv"
-LOG_DIR="/home/sidrees/scratch/RetinaPredictors/grid_scripts/logs"
+LOG_DIR="/home/sidrees/scratch/RetinaPredictors_MOV/grid_scripts/logs"
 
 if [ ! -d "$LOG_DIR" ]; then 
 	mkdir $LOG_DIR
@@ -41,6 +41,7 @@ MaxPool_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f21) )
 num_trials_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f22) )
 use_chunker_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f23 )
 TRSAMPS_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f24) )
+lr_all=( $(tail -n +2 $PARAMS_FILE | cut -d ',' -f25) )
 
 
 numParams=${#expDate_all[@]}
@@ -82,6 +83,7 @@ do
  typeset -i num_trials=${num_trials_all[i]}
  typeset -i use_chunker=${use_chunker_all[i]}
  typeset -i TRSAMPS=${TRSAMPS_all[i]}
+ typeset -i lr=${lr_all[i]}
 
  
 
@@ -109,18 +111,19 @@ do
  echo "num_trials: $num_trials" 
  echo "USE_CHUNKERS: $use_chunker" 
  echo "TRSAMPS: $TRSAMPS" 
+ echo "Learning rate: $lr"
  
  for ((t=1; t<$num_trials+1; t++));
  do
   typeset -i c_trial=$t
-  JOB_ID=$(sbatch --export=LOG_DIR=$LOG_DIR,expDate=$expDate,mdl_name=$mdl_name,path_model_save_base=$path_model_save_base,fname_data_train_val_test=$fname_data_train_val_test,path_existing_mdl=$path_existing_mdl,runOnCluster=$runOnCluster,chan1_n=$chan1_n,filt1_size=$filt1_size,filt1_3rdDim=$filt1_3rdDim,chan2_n=$chan2_n,filt2_size=$filt2_size,filt2_3rdDim=$filt2_3rdDim,chan3_n=$chan3_n,filt3_size=$filt3_size,filt3_3rdDim=$filt3_3rdDim,nb_epochs=$nb_epochs,thresh_rr=$thresh_rr,temporal_width=$temporal_width,pr_temporal_width=$pr_temporal_width,bz_ms=$bz_ms,BatchNorm=$BatchNorm,MaxPool=$MaxPool,c_trial=$c_trial,use_chunker=$use_chunker,TRSAMPS=$TRSAMPS cnn3d_launcher.sh)
+  JOB_ID=$(sbatch --export=LOG_DIR=$LOG_DIR,expDate=$expDate,mdl_name=$mdl_name,path_model_save_base=$path_model_save_base,fname_data_train_val_test=$fname_data_train_val_test,path_existing_mdl=$path_existing_mdl,runOnCluster=$runOnCluster,chan1_n=$chan1_n,filt1_size=$filt1_size,filt1_3rdDim=$filt1_3rdDim,chan2_n=$chan2_n,filt2_size=$filt2_size,filt2_3rdDim=$filt2_3rdDim,chan3_n=$chan3_n,filt3_size=$filt3_size,filt3_3rdDim=$filt3_3rdDim,nb_epochs=$nb_epochs,thresh_rr=$thresh_rr,temporal_width=$temporal_width,pr_temporal_width=$pr_temporal_width,bz_ms=$bz_ms,BatchNorm=$BatchNorm,MaxPool=$MaxPool,c_trial=$c_trial,use_chunker=$use_chunker,TRSAMPS=$TRSAMPS,lr=$lr cnn3d_launcher.sh)
  
   echo $JOB_ID
-  JOB_ID=$(echo "$JOB_ID" | grep -Eo '[0-9]{1,7}')
+  JOB_ID=$(echo "$JOB_ID" | grep -Eo '[0-9]{1,8}')
  
 #  echo "JOB ID: $A\n\nexpDate: $expDate\nthresh_rr: $thresh_rr\ntemporal_width: $temporal_width\nbz_ms: $bz_ms\nnb_epochs: $nb_epochs\nchan1_n: $chan1_n\nfilt1_size: $filt1_size\nfilt1_3rdDim: $filt1_3rdDim\nfilt1_3rdDim: $filt1_3rdDim\nchan2_n: $chan2_n\nfilt2_size: $filt2_size\nfilt2_3rdDim: $filt2_3rdDim\nchan3_n: $chan3_n\nfilt3_size: $filt3_size\nfilt3_3rdDim: $filt3_3rdDim\nBatchNorm=$BatchNorm\nMaxPool=$MaxPool\nc_trial: $c_trial" > $LOG_DIR/$JOB_ID-out.txt
 
-  printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t' $JOB_ID $expDate $mdl_name $path_model_save_base $fname_data_train_val_test $path_existing_mdl $thresh_rr $temporal_width $pr_temporal_width $bz_ms $nb_epochs $chan1_n $filt1_size $filt1_3rdDim $chan2_n $filt2_size $filt2_3rdDim $chan3_n $filt3_size $filt3_3rdDim $BatchNorm $MaxPool $c_trial $use_chunker $TRSAMPS | paste -sd '\t' >> job_list.csv
+  printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t' $JOB_ID $expDate $mdl_name $path_model_save_base $fname_data_train_val_test $path_existing_mdl $thresh_rr $temporal_width $pr_temporal_width $bz_ms $nb_epochs $chan1_n $filt1_size $filt1_3rdDim $chan2_n $filt2_size $filt2_3rdDim $chan3_n $filt3_size $filt3_3rdDim $BatchNorm $MaxPool $c_trial $use_chunker $TRSAMPS $lr | paste -sd '\t' >> job_list.csv
  
  done
  
